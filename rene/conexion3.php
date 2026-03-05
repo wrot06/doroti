e44f5d11e0d205ae4935e3fc52800bf8048e2144
wrot06 <wrot06@hotmail.com>
2026-03-05 08:30:16 -0500

    antes de crear la sub series

--- rene/conexion3.php
+++ rene/conexion3.php
@@ -3,9 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = 'localhost';
+$host = 'sql103.infinityfree.com';
 $usuario = 'if0_38210727';
-$contraseña = '1234';
+$contraseña = 'yBxDZHWJ45vhR';
 $base_datos = 'if0_38210727_doroti';
 
 $conec = new mysqli($host, $usuario, $contraseña, $base_datos);
